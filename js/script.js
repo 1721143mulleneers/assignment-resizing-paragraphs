7e5876c724304e9507aefee890a431e627d194ab
Emmy Mulleneers <61006428+1721143mulleneers@users.noreply.github.com>
2020-03-18 14:56:00 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let sizes = [4, 8, 15, 16, 23, 42];
+
+d3.selectAll('.paragraph')
+    .data(sizes)
+    .style("font-size", (size) => `${size}px`);
+
+
+d3.select('#noParagraphsPresent')
+    .selectAll('p')
+    .data(sizes)
+    .enter()
+    .append('p')
+    .text('Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa impedit debitis expedita explicabo doloremque animi, quisquam obcaecati ipsum nobis in.')
+    .style('font-size', (size) => `${size}px`)
